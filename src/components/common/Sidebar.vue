0f90fdfba3c3341cbb20fc99df0c1bd3c7a4a5a2
gold <291817075@qq.com>
2019-03-18 16:26:33 +0800

    完善

--- src/components/common/Sidebar.vue
+++ src/components/common/Sidebar.vue
@@ -54,35 +54,6 @@
                         title: '警告消息'
                     },
                     // {
-                    //     icon: 'el-icon-lx-calendar',
-                    //     index: '3',
-                    //     title: '表单相关',
-                    //     subs: [
-                    //         {
-                    //             index: 'form',
-                    //             title: '基本表单'
-                    //         },
-                    //         {
-                    //             index: '3-2',
-                    //             title: '三级菜单',
-                    //             subs: [
-                    //                 {
-                    //                     index: 'editor',
-                    //                     title: '富文本编辑器'
-                    //                 },
-                    //                 {
-                    //                     index: 'markdown',
-                    //                     title: 'markdown编辑器'
-                    //                 },
-                    //             ]
-                    //         },
-                    //         {
-                    //             index: 'upload',
-                    //             title: '文件上传'
-                    //         }
-                    //     ]
-                    // },
-                    // {
                     //     icon: 'el-icon-lx-emoji',
                     //     index: 'icon',
                     //     title: '自定义图标'
@@ -92,36 +63,21 @@
                         index: 'charts',
                         title: '可视化图表'
                     },
-                    // {
-                    //     icon: 'el-icon-rank',
-                    //     index: '6',
-                    //     title: '拖拽组件',
-                    //     subs: [
-                    //         {
-                    //             index: 'drag',
-                    //             title: '拖拽列表',
-                    //         },
-                    //         {
-                    //             index: 'dialog',
-                    //             title: '拖拽弹框',
-                    //         }
-                    //     ]
-                    // },
-                    // {
-                    //     icon: 'el-icon-lx-warn',
-                    //     index: '7',
-                    //     title: '错误处理',
-                    //     subs: [
-                    //         {
-                    //             index: 'permission',
-                    //             title: '权限测试'
-                    //         },
-                    //         {
-                    //             index: '404',
-                    //             title: '404页面'
-                    //         }
-                    //     ]
-                    // }
+                    {
+                        icon: 'el-icon-lx-warn',
+                        index: '7',
+                        title: '错误处理',
+                        subs: [
+                            {
+                                index: 'permission',
+                                title: '权限测试'
+                            },
+                            {
+                                index: '404',
+                                title: '404页面'
+                            }
+                        ]
+                    }
                 ]
             }
         },
--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css'; // 默认主题
 // import '../static/css/theme-green/index.css';       // 浅绿色主题
 import './assets/css/icon.css';
-import './components/common/directives';
 import "babel-polyfill";
 
 Vue.use(VCharts)
